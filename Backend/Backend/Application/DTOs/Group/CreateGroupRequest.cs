5c47f14ca0d6c29c5a317a7ae46bd7fa11ec9582
Toàn Lê <toanmadrid99@gmail.com>
2021-05-17 21:58:03 +0700

    add required annotation for requests

--- Backend/Backend/Application/DTOs/Group/CreateGroupRequest.cs
+++ Backend/Backend/Application/DTOs/Group/CreateGroupRequest.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Application.DTOs.Group
 {
     public class CreateGroupRequest
     {
+        [Required]
         public string GroupName { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Rules { get; set; }
     }
 }
