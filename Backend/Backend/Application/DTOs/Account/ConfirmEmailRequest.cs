b7886947fd05fbb2abcff73305151eed6a12d726
Toàn Lê <lmtoan99@gmail.com>
2021-05-17 21:59:25 +0700

    add required annotation for requests (#11)
    Co-authored-by: Toàn Lê <toanmadrid99@gmail.com>

--- Backend/Backend/Application/DTOs/Account/ConfirmEmailRequest.cs
+++ Backend/Backend/Application/DTOs/Account/ConfirmEmailRequest.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Application.DTOs.Account
 {
     public class ConfirmEmailRequest
     {
+        [Required]
         public string UserId { get; set; }
+        [Required]
         public string Code { get; set; }
     }
 }
