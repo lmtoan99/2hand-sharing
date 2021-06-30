34339cb4af8650c83f072e3fb16ff5cadb31d38b
Toàn Lê <toanmadrid99@gmail.com>
2021-06-30 23:11:45 +0700

    SearchDonateItem

---
+++ Backend/Backend/Persistence/Migrations/20210630112059_AddSearchFullTextIndexOnItemsTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddSearchFullTextIndexOnItemsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(sql: "ALTER TABLE Items ADD FULLTEXT Index_Items(ItemName) WITH PARSER NGRAM", suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(sql: "DROP INDEX ON Items(ItemName) KEY INDEX Index_Items;", suppressTransaction: true);
+        }
+    }
+}
