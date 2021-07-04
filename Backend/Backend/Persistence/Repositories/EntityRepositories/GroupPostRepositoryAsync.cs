87bfef6a2841e2b450b056fd3d80e112a67382bc
dtkng <dothikimngan2511@gmail.com>
2021-07-04 23:11:40 +0700

    update create post and get all post

--- Backend/Backend/Persistence/Repositories/EntityRepositories/GroupPostRepositoryAsync.cs
+++ Backend/Backend/Persistence/Repositories/EntityRepositories/GroupPostRepositoryAsync.cs
@@ -1,10 +1,13 @@
-﻿using Application.Interfaces.Repositories;
+﻿using Application.Enums;
+using Application.Interfaces.Repositories;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Context;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Persistence.Repositories.EntityRepositories
 {
@@ -17,5 +20,16 @@ namespace Persistence.Repositories.EntityRepositories
             _groupPost = dbContext.Set<GroupPost>();
         }
 
+        public async Task<IReadOnlyList<GroupPost>> GetAllPublicPostInGroupAsync(int pageNumber, int pageSize, int groupId)
+        {
+            return await _groupPost
+                .Where(e => e.GroupId == groupId && e.Visibility == (int)GroupPostType.PUBLIC_POST)
+                .OrderByDescending(i => i.PostTime)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Include(i => i.PostByAccount)
+                .Include(i => i.GroupPostImageRelationships)
+                .ToListAsync();
+        }
     }
 }
