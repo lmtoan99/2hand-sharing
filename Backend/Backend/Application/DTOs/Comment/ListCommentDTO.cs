2bff29dc78d2b4fdcdeb08c4fd43e4cd65c10316
meokg456 <meokg456@gmail.com>
2021-07-06 19:05:31 +0700

    + Search by category id

--- Backend/Backend/Application/DTOs/Comment/ListCommentDTO.cs
+++ Backend/Backend/Application/DTOs/Comment/ListCommentDTO.cs
@@ -4,14 +4,9 @@ using System.Text;
 
 namespace Application.DTOs.Comment
 {
-    public class ListCommentDTO 
+    public class ListCommentDTO : CommentDTO
     {
         public string AvatarUrl { get; set; }
         public string PostByAccountName { get; set; }
-        public int Id { get; set; }
-        public int PostId { get; set; }
-        public string Content { get; set; }
-        public int PostByAccontId { get; set; }
-        public DateTime PostTime { get; set; }
     }
 }
