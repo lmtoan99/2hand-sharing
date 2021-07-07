1ad465e3c53111cb13d04c051b134964aed571f8
Toàn Lê <toanmadrid99@gmail.com>
2021-07-07 16:46:45 +0700

    UpdateItem

---
+++ Backend/Backend/Application/DTOs/Item/UpdateItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Address;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.DTOs.Item
+{
+    public class UpdateItemDTO
+    {
+        public string ItemName { get; set; }
+        public AddressDTO ReceiveAddress { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public int ImageNumber { get; set; }
+        public List<string> DeletedImages { get; set; }
+    }
+}
