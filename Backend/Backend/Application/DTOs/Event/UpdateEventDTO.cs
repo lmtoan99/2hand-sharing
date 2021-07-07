210ebc22393dded6fd20e7d5bb368e320e22060e
dtkng <dothikimngan2511@gmail.com>
2021-07-07 19:07:49 +0700

    update event

---
+++ Backend/Backend/Application/DTOs/Event/UpdateEventDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Event
+{
+    public class UpdateEventDTO
+    {
+        public string EventName { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Content { get; set; }
+    }
+}
