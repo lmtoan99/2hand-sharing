967ecf9a16c850f003d05a0e6cea07054d683500
dtkng <dothikimngan2511@gmail.com>
2021-07-07 16:33:00 +0700

    update group

---
+++ Backend/Backend/Application/DTOs/Group/UpdateGroupDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Group
+{
+    public class UpdateGroupDTO
+    {
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public string Rules { get; set; }
+    }
+}
